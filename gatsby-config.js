6fc0ce49dce692864ceac72bafa307a6ea045a46
evanjjones <evanjones@fastmail.net>
2021-05-11 11:38:33 -0400

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: "its-a-rock-fact",
   },
-  plugins: ["gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-react-helmet", {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `GatsbyJS`,
+      short_name: `GatsbyJS`,
+      start_url: `/`,
+      background_color: `#f7f0eb`,
+      theme_color: `#a2466c`,
+      display: `standalone`,
+      icon: `src/images/icon.png`
+    }}],
 };
